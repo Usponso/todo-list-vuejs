75841210eb812615a0ded32f61b1804608ffa5a0
Enzo <enaverlant@gmail.com>
2021-12-21 13:24:28 +0100

    modif: Mise a jour nom fichier

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label>
+      Nom de la tâche
+    </label>
+    <input type="text" name="new-tache" v-model="nomTache" />
+    <ajouterBouton class="btnAjouter" @ajouterTache="ajouter"/>
+
+    <h2>Tâches :</h2>
+    <ul>
+      <li v-for="(tache, index) in taches" :key="tache.name"><input type="checkbox" v-model="tache.checked"/> 
+      <div :class="{termine: tache.checked}">{{ tache.name }}</div>
+      <button @click="supprimer(index)" class="btnSupprimer">Supprimer</button></li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ajouterBouton from "./components/AjouterBouton.vue"
 
 export default {
   name: 'App',
+  data() {
+    return {
+      check: "",
+      nomTache: "",
+      newTache: {name: "", checked: ""},
+      taches: []
+    }
+  },
   components: {
-    HelloWorld
+    ajouterBouton,
+  },
+  methods: {
+    ajouter(){
+      this.taches.push({name: this.nomTache, checked: ""})
+    },
+    supprimer(index){
+      this.taches.splice(index,1)
+    }
   }
 }
 </script>
@@ -22,7 +48,42 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
+}
+input[type="text"]{
+  padding: 7px;
+  border-radius: 5px;
+  border: 1px solid darkgrey;
+}
+ul{
+  list-style: none;
+}
+li{
+  display: flex;
+  place-content: space-evenly;
+  padding: 5px;
+}
+.termine{
+  color: grey;
+  text-decoration: line-through;
+  text-decoration-color: red;
+  font-style: italic;
+}
+.btnAjouter{
+  padding: 10px;
+  background-color: green;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+}
+.btnSupprimer{
+  padding: 10px;
+  background-color: red;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/AjouterBouton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="emitAjouter">Ajouter</button>
+</template>
+
+<script>
+export default{
+    name: "AjouterBouton",
+    methods: {
+        emitAjouter(){
+            this.$emit("ajouterTache")
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
